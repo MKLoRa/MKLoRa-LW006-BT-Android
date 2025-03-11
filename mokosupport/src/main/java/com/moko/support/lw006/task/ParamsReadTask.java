7a04c62cf3c915ab8f6d0dca90aee78ce7e05722
liuwenzheng <329541594@qq.com>
2025-03-11 12:13:46 +0800

    修改选择MOKOIoT平台时，CH和DR初始范围更新

--- mokosupport/src/main/java/com/moko/support/lw006/task/ParamsReadTask.java
+++ mokosupport/src/main/java/com/moko/support/lw006/task/ParamsReadTask.java
@@ -1,8 +1,15 @@
 package com.moko.support.lw006.task;
 
+import android.text.TextUtils;
+
 import com.moko.ble.lib.task.OrderTask;
+import com.moko.ble.lib.utils.MokoUtils;
+import com.moko.support.lw006.LoRaLW006MokoSupport;
 import com.moko.support.lw006.entity.OrderCHAR;
 import com.moko.support.lw006.entity.ParamsKeyEnum;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class ParamsReadTask extends OrderTask {
     public byte[] data;
@@ -38,5 +45,60 @@ public class ParamsReadTask extends OrderTask {
                 (byte) 0x00
         };
         response.responseValue = data;
+    }
+
+
+    private int packetCount;
+    private int packetIndex;
+    private int dataLength;
+    private byte[] dataBytes;
+    private StringBuilder dataSb;
+
+    @Override
+    public boolean parseValue(byte[] value) {
+        final int header = value[0] & 0xFF;
+        final int flag = value[1] & 0xFF;
+        if (header == 0xEE && flag == 0x00) {
+            // 分包读取时特殊处理
+            final int cmd = value[2] & 0xFF;
+            packetCount = value[3] & 0xFF;
+            packetIndex = value[4] & 0xFF;
+            final int length = value[5] & 0xFF;
+            if (packetIndex == 0) {
+                // 第一包
+                dataLength = 0;
+                dataSb = new StringBuilder();
+            }
+            ParamsKeyEnum keyEnum = ParamsKeyEnum.fromParamKey(cmd);
+            if (Objects.requireNonNull(keyEnum) == ParamsKeyEnum.KEY_FILTER_NAME_RULES) {
+                if (length > 0) {
+                    dataLength += length;
+                    byte[] responseData = Arrays.copyOfRange(value, 6, 6 + length);
+                    dataSb.append(MokoUtils.bytesToHexString(responseData));
+                }
+                if (packetIndex == (packetCount - 1)) {
+                    if (!TextUtils.isEmpty(dataSb.toString()))
+                        dataBytes = MokoUtils.hex2bytes(dataSb.toString());
+                    byte[] responseValue = new byte[4 + dataLength];
+                    responseValue[0] = (byte) 0xED;
+                    responseValue[1] = (byte) 0x00;
+                    responseValue[2] = (byte) cmd;
+                    responseValue[3] = (byte) dataLength;
+                    for (int i = 0; i < dataLength; i++) {
+                        responseValue[4 + i] = dataBytes[i];
+                    }
+                    dataSb = null;
+                    dataBytes = null;
+                    // 最后一包
+                    orderStatus = 1;
+                    response.responseValue = responseValue;
+                    LoRaLW006MokoSupport.getInstance().pollTask();
+                    LoRaLW006MokoSupport.getInstance().executeTask();
+                    LoRaLW006MokoSupport.getInstance().orderResult(response);
+                }
+            }
+            return false;
+        }
+        return true;
     }
 }
